ae8cd13a2d144b27db4599764fb64886971fe26d
Vasyl Vintoniv <126397140+vinvas94@users.noreply.github.com>
2024-05-28 22:05:16 +0200

    Update contactsRouter.js

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -8,16 +8,16 @@ import schemas from "../schemas/contactsSchemas.js";
 
 const contactsRouter = express.Router();
 
-contactsRouter.get("/", ctrl.listContacts);
+contactsRouter.get("/", ctrl.getAllContacts);
 
-contactsRouter.get("/:id", ctrl.getContactById);
+contactsRouter.get("/:id", ctrl.getOneContact);
 
-contactsRouter.delete("/:id", ctrl.removeContact);
+contactsRouter.delete("/:id", ctrl.deleteContact);
 
 contactsRouter.post(
   "/",
   validateBody(schemas.createContactSchema),
-  ctrl.addContact
+  ctrl.createContact
 );
 
 contactsRouter.put(
